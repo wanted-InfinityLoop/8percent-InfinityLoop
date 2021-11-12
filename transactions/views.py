5a01d7e699ec5a4ebe33a5a488f20e0613a228aa
heejung-gjt <hee_9803@naver.com>
2021-11-12 12:46:45 +0900

    feat: post deposit function
    - deposit 유닛테스트 구현
    - deposit code line 변경

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http.response import JsonResponse
+from django.views import View
+from django.db import transaction
+
+from transactions.models import *
+from users.models import *
+
+
+class DepositView(View):
+    def post(self, request):
+        try:
+            data    = json.loads(request.body)
+            deposit = User.objects.get(id=1).deposit # request.user.deposit
+            
+            if data["amounts"] <= 0:
+                return JsonResponse({"message": "Invalid Amount"}, status=400)
+
+            with transaction.atomic():
+                transaction_info = Transaction.objects.create(
+                    type_id      = TransactionType.Type.DEPOSIT.value,
+                    information  = data["information"],
+                    amounts      = data["amounts"],
+                    balance      = deposit.balance + data["amounts"],
+                    user_id      = 1 # request.user가능
+                )
+
+                deposit.balance += data["amounts"]
+                deposit.save()
+
+            return JsonResponse({"message"                 : "Success",
+                                "user balance"             : deposit.balance,
+                                "user transaction type"    : transaction_info.type.name,
+                                "user transaction amounts" : transaction_info.amounts,
+                                "user transactions balance": transaction_info.balance
+                                }, status=201)
+
+        except KeyError:    
+            return JsonResponse({"message": "Key Error"}, status=400)
+
+        except TypeError:
+            return JsonResponse({"message": "Type Error"}, status=400)

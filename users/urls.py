b23a85edd241c987885af63f5ee7f896000208e8
dongheon yu <ptw880@gmail.com>
2021-11-12 17:15:36 +0900

    feat: signin function
    - 로그인 기능 구현
    - 로그인 테스트 코드 작성
    - 데코레이터 작성

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views      import SignUpView
+from .views      import SignUpView, SigninView, Example
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
-    # path('/signin', SigninView.as_view()),
-    # path('/decorator-test', Example.as_view())
+    path('/signin', SigninView.as_view()),
 ]

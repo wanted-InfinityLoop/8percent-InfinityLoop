ddfe54763203c47d2094d95bf29a90189fd70c23
dongheon yu <ptw880@gmail.com>
2021-11-12 13:23:24 +0900

    feat: create signup view
    - 회원가입 기능 구현
    - validation line 수정
    - users views line 수정

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,129 @@
-from django.test import TestCase
+import json
+import bcrypt
+import jwt
+import unittest
+import re
 
-# Create your tests here.
+from django.http         import response
+from django.test         import TestCase, Client
+from codecs              import encode, decode
+from transactions.models import Deposit, Bank
+from users.models        import User
+from my_settings         import MY_SECRET_KEY
+from core.validation     import algorithm
+
+client = Client()
+
+class SignUpViewTest(TestCase):
+    
+    def setUp(self):
+        
+        Bank.objects.create(
+            id   = 1,
+            name = "NH_BANK"
+        )
+        
+        Deposit.objects.create(
+            id                 = 1,
+            withdrawal_account = "111122223333",
+            withdrawal_bank_id = 1,
+            deposit_account    = "444433332222",
+            deposit_bank_id    = 1,
+            balance            = 0
+        )
+        
+        User.objects.create(
+            id   = 1,
+            name = "전소민",
+            email = "junsomin@gmail.com",
+            password = "aa12341234!",
+            deposit = Deposit.objects.get(id=1)
+        )
+        
+    def tearDown(self):
+        User.objects.all().delete()
+        Deposit.objects.all().delete()
+        Bank.objects.all().delete()
+        
+    def test_succuss_signup(self):
+        
+        data = {
+            "name"           : "이광수",
+            "email"          : "gwangsu@gmail.com",
+            "password"       : "aa12341234!",
+            "bank_name"      : 2,
+            "account_number" : "124239429421"
+        }
+        
+        response = client.post("/users/signup", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message" : "SUCCESS"})
+        
+    def test_fail_signup_exists_email(self):
+        
+        data = {
+            "name"           : "이광수",
+            "email"          : "junsomin@gmail.com",
+            "password"       : "aa12341234!",
+            "bank_name"      : 2,
+            "account_number" : "124239429421"
+        }
+        
+        response = client.post("/users/signup", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'EXIST_EMAIL'})
+        
+    def test_fail_signup_not_matched_email_from(self):
+        
+        data = {
+            "name"           : "이광수",
+            "email"          : "gwangsugmail.com",
+            "password"       : "aa12341234!",
+            "bank_name"      : 2,
+            "account_number" : "124239429421"
+        }
+        
+        response = client.post("/users/signup", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'NOT_MATCHED_EMAIL_FORM'})
+
+    def test_fail_signup_not_matched_password_from(self):
+        
+        data = {
+            "name"           : "이광수",
+            "email"          : "gwangsu@gmail.com",
+            "password"       : "aa12341234",
+            "bank_name"      : 2,
+            "account_number" : "124239429421"
+        }
+        
+        response = client.post("/users/signup", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'NOT_MATCHED_PASSWORD_FORM'})
+        
+    def test_fail_signup_not_input_account(self):
+        
+        data = {
+            "name"           : "이광수",
+            "email"          : "gwangsu@gmail.com",
+            "password"       : "aa12341234!",
+            "bank_name"      : 2,
+            "account_number" : ""
+        }
+        
+        response = client.post("/users/signup", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'NOT_INPUT_ACCOUNT'})
+        
+    def test_fail_signup_not_input_account(self):
+        
+        data = {
+            "name"           : "이광수",
+            "email"          : "gwangsu@gmail.com",
+            "password"       : "aa12341234!",
+            "bank_name"      : 2,
+        }
+        
+        response = client.post("/users/signup", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message" : "KEY_ERROR"})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import re
+from django import views
+import jwt
 
-# Create your views here.
+from django.views        import View
+from django.http         import HttpResponse, JsonResponse
+from users.models        import User
+from transactions.models import Bank, Deposit, Transaction
+from my_settings         import MY_SECRET_KEY
+from core.validation     import email_validation, password_validation
+from uuid                import uuid4
+from django.db           import transaction
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            with transaction.atomic():
+                data               = json.loads(request.body)
+            
+                email              = data["email"]
+                password           = data["password"]
+                name               = data["name"]
+                withdrawal_account = data["account_number"]
+        
+                if User.objects.filter(email=email).exists():
+                    return JsonResponse({'message' : 'EXIST_EMAIL'}, status=400)
+        
+                if not email_validation.match(email):
+                    return JsonResponse({'message' : 'NOT_MATCHED_EMAIL_FORM'}, status=400)
+            
+                if not password_validation.match(password):
+                    return JsonResponse({'message' : 'NOT_MATCHED_PASSWORD_FORM'}, status=400)
+                
+                if not withdrawal_account:
+                    return JsonResponse({'message' : 'NOT_INPUT_ACCOUNT'}, status=400)
+            
+                bank_created = Bank.objects.get_or_create(name=data["bank_name"])
+            
+                deposit_created = Deposit.objects.create(
+                    withdrawal_account = withdrawal_account,
+                    withdrawal_bank    = bank_created[0],
+                    deposit_account    = str(uuid4().int)[:14],
+                    deposit_bank_id    = Bank.DefaultBank.NH_BANK.value
+                )
+    
+                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+                User.objects.create(
+                    email    = email,
+                    name     = name,
+                    password = hashed_password.decode("utf-8"),
+                    deposit  = deposit_created
+                    )
+                
+                return JsonResponse({"message" : "SUCCESS"}, status=200)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)

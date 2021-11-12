a1fa139fded9848f4ba137ef8ae6abecedd199b3
YeJun <hayejun1013@naver.com>
2021-11-12 18:00:19 +0900

    feat: add transactions list GET method
    - 거래내역 GET 함수 구현
    - 입금, 출금, 또는 기간으로 조회 가능
    - Pagination 구현
    - 총 거래내역 및 거래 유형 별 거래 금액 합산 정보 제공
    refactor: refactor view code

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
-from django.urls      import path
-from django.urls.conf import include
+from django.urls import path, include
 
 urlpatterns = [
-    path("users", include('users.urls'))
+    path("users", include('users.urls')),
+    path("transactions", include("transactions.urls")),
 ]

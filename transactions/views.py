ad6aa96405ff36db70daafb8353abbab3afb0d2c
Oderystella <ddott59@gmail.com>
2021-11-12 15:55:24 +0900

    feat: add withdral post function
    - 출금 API 구현
    - 로그인 API 완성되면 @login_decorator 붙여주고, 필요없는 코드 삭제 예정
    - 출금 성공 테스트
    - 출금액이 0 이하일 때 테스트
    - 인풋이 없을 때 테스트
    - 출금액이 계좌 잔고를 초과했을 때 테스트
    - 타입 에러 테스트
    - 키 에러 테스트
    - 유저 없을 때 테스트(재확인 필요)

--- transactions/views.py
+++ transactions/views.py
@@ -1,8 +1,8 @@
 import json
 
-from django.http.response import JsonResponse
-from django.views import View
-from django.db import transaction
+from django.http      import JsonResponse
+from django.views     import View
+from django.db        import transaction
 
 from transactions.models import *
 from users.models import *
@@ -41,3 +41,49 @@ class DepositView(View):
 
         except TypeError:
             return JsonResponse({"message": "Type Error"}, status=400)
+
+
+class WithdrawalView(View):
+
+    def post(self, request):
+        try:
+            data             = json.loads(request.body)
+            amounts          = data["amounts"]
+            information      = data["information"]
+            # user             = request.user
+            user = User.objects.get(id=2)   # 추후 삭제할 예정
+
+            if amounts <= 0:
+                return JsonResponse({"message": "Invalid Input"}, status=400)
+            
+            if not amounts or not information:
+                return JsonResponse({"message": "No Input"}, status=400)
+
+            # if not user:
+            #     return JsonResponse({"message": "User Does Not Exist"}, status=400)
+
+            if amounts > user.deposit.balance:
+                return JsonResponse({"message": "Wrong Request"}, status=400)
+
+            with transaction.atomic():
+                new_transaction = Transaction.objects.create(
+                    type_id    =TransactionType.Type.WITHDRAWAL.value,
+                    information=information,
+                    amounts    =amounts,
+                    balance    =user.deposit.balance - amounts,
+                    user       =user
+                )
+                user.deposit.balance = new_transaction.balance
+                user.deposit.save()
+
+            return JsonResponse({"message": "Success", "거래 후 잔액": new_transaction.balance}, status=201)
+
+        except TypeError:
+            return JsonResponse({"message": "Type Error"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"message": "Key Error"}, status=400)
+        
+        except User.DoesNotExist:
+            return JsonResponse({"message":"User Does Not Exist"}, status=400)  # login_decorator 붙이면 삭제하기
+            

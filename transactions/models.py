a69c1c846e6aff7a5110cc4083660c75a3c6961e
YeJun <hayejun1013@naver.com>
2021-11-12 22:50:00 +0900

    refactor: refactor source codes
    - transactions 앱 리팩토링
    - users 앱 리팩토링
    - core 앱 리팩토링
    - 테스트 코드 리팩토링

--- transactions/models.py
+++ transactions/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db   import models
 
 from core.models import TimeStamp
 
@@ -15,10 +15,10 @@ class Bank(models.Model):
 
 class Deposit(models.Model):
     withdrawal_account = models.CharField(max_length=32)
-    withdrawal_bank = models.ForeignKey(Bank, related_name="deposit_withdrawal", on_delete=models.PROTECT)
-    deposit_account = models.CharField(max_length=32, unique=True)
-    deposit_bank = models.ForeignKey(Bank, related_name="deposit", on_delete=models.PROTECT)
-    balance = models.PositiveBigIntegerField(default=0)
+    withdrawal_bank    = models.ForeignKey(Bank, related_name="deposit_withdrawal", on_delete=models.PROTECT)
+    deposit_account    = models.CharField(max_length=32, unique=True)
+    deposit_bank       = models.ForeignKey(Bank, related_name="deposit", on_delete=models.PROTECT)
+    balance            = models.PositiveBigIntegerField(default=0)
 
     class Meta:
         db_table = "deposit"
@@ -26,7 +26,7 @@ class Deposit(models.Model):
 
 class TransactionType(models.Model):
     class Type(models.IntegerChoices):
-        DEPOSIT = 1
+        DEPOSIT    = 1
         WITHDRAWAL = 2
 
     name = models.CharField(max_length=16)
@@ -36,11 +36,11 @@ class TransactionType(models.Model):
 
 
 class Transaction(TimeStamp):
-    type = models.ForeignKey(TransactionType, on_delete=models.PROTECT)
+    type        = models.ForeignKey(TransactionType, on_delete=models.PROTECT)
     information = models.CharField(max_length=64)
-    amounts = models.PositiveIntegerField()
-    balance = models.PositiveBigIntegerField()
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    amounts     = models.PositiveIntegerField()
+    balance     = models.PositiveBigIntegerField()
+    user        = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "transactions"
--- users/views.py
+++ users/views.py
@@ -1,85 +1,72 @@
 import json
 import bcrypt
-import re
-from django import views
 import jwt
+from uuid import uuid4
 
-from django.views        import View
-from django.http         import HttpResponse, JsonResponse
-from users.models        import User
-from transactions.models import Bank, Deposit, Transaction
-from my_settings         import MY_SECRET_KEY
-from core.validation     import email_validation, password_validation, algorithm
-from uuid                import uuid4
-from django.db           import transaction
-from core.utils          import LoginDecorator
+from django.db import transaction
+from django.views import View
+from django.http import JsonResponse
+
+from users.models import User
+from transactions.models import Bank, Deposit
+from my_settings import MY_SECRET_KEY
+from core.validation import email_validation, password_validation, algorithm
+
 
 class SignUpView(View):
     def post(self, request):
         try:
             with transaction.atomic():
-                data               = json.loads(request.body)
-            
-                email              = data["email"]
-                password           = data["password"]
-                name               = data["name"]
-                withdrawal_account = data["account_number"]
-        
-                if User.objects.filter(email=email).exists():
-                    return JsonResponse({'message' : 'EXIST_EMAIL'}, status=400)
-        
-                if not email_validation.match(email):
-                    return JsonResponse({'message' : 'NOT_MATCHED_EMAIL_FORM'}, status=400)
-            
-                if not password_validation.match(password):
-                    return JsonResponse({'message' : 'NOT_MATCHED_PASSWORD_FORM'}, status=400)
-                
-                if not withdrawal_account:
-                    return JsonResponse({'message' : 'NOT_INPUT_ACCOUNT'}, status=400)
-            
+                data = json.loads(request.body)
+                password = data["password"]
+
+                if User.objects.filter(email=data["email"]).exists():
+                    return JsonResponse({"message": "DUPLICATED_EMAIL"}, status=400)
+
+                if not email_validation.match(data["email"]):
+                    return JsonResponse({"message": "EMIAL_FORMAT_ERROR"}, status=400)
+
+                if not password_validation.match(data["password"]):
+                    return JsonResponse({"message": "PASSWORD_FORMAT_ERROR"}, status=400)
+
                 bank_created = Bank.objects.get_or_create(name=data["bank_name"])
-            
+
                 deposit_created = Deposit.objects.create(
-                    withdrawal_account = withdrawal_account,
-                    withdrawal_bank    = bank_created[0],
-                    deposit_account    = str(uuid4().int)[:14],
-                    deposit_bank_id    = Bank.DefaultBank.NH_BANK.value
+                    withdrawal_account=data["account_number"],
+                    withdrawal_bank   =bank_created[0],
+                    deposit_account   =str(uuid4().int)[:14],
+                    deposit_bank_id   =Bank.DefaultBank.NH_BANK.value,
                 )
-    
-                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
                 User.objects.create(
-                    email    = email,
-                    name     = name,
-                    password = hashed_password.decode("utf-8"),
-                    deposit  = deposit_created
-                    )
-                
-                return JsonResponse({"message" : "SUCCESS"}, status=200)
-        
+                    email   =data["email"],
+                    name    =data["name"],
+                    password=bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8"),
+                    deposit =deposit_created,
+                )
+
+                return JsonResponse({"message": "SUCCESS"}, status=201)
+
         except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
 
 class SigninView(View):
     def post(self, request):
-        
         try:
-            data     = json.loads(request.body)
-            
-            email    = data["email"]
-            password = data["password"]
-            
-            user_check = User.objects.filter(email=email).first()
-            
-            if not user_check:
-                return JsonResponse({"message" : "INVALED_USER"}, status=400)
-            
-            if not bcrypt.checkpw(password.encode("utf-8"), user_check.password.encode("utf-8")):
-                return JsonResponse({"message" : "INVALID_USER_PASSWORD"}, status=400)
-            
-            access_token = jwt.encode({"user_id" : user_check.id}, MY_SECRET_KEY, algorithm)
-            
-            return JsonResponse({"message" : access_token}, status=200)
-        
+            data = json.loads(request.body)
+
+            if not User.objects.filter(email=data["email"]).exists():
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            user = User.objects.get(email=data["email"])
+
+            if not bcrypt.checkpw(data["password"].encode("utf-8"), user.password.encode("utf-8")):
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            access_token = jwt.encode({"user_id": user.id}, MY_SECRET_KEY, algorithm)
+
+            return JsonResponse({"message": "SUCCESS", "token": access_token}, status=200)
+
         except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
